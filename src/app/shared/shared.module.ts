5527ef6478af7d2f661f44f6f938e720c859de2c
Mohamed <mshareef@kiitos-tech.com>
2020-08-30 00:21:55 +0300

    create full input component with copy to clipboard feature #17

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { InputComponent } from './input/input.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [ButtonComponent, InputComponent],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    ClipboardModule,
+    MatTooltipModule,
   ],
-  exports: [ButtonComponent],
+  exports: [
+    ButtonComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule { }
