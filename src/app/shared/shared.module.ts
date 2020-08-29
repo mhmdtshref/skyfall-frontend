038612e24ea615c58f27043ab714a0eb751c0dd6
Mohamed <mshareef@kiitos-tech.com>
2020-08-29 22:12:39 +0300

    Create initial input component #17

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { InputComponent } from './input/input.component';
 
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, InputComponent],
   imports: [
     CommonModule,
     MatButtonModule,
