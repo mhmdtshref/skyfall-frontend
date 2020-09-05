ed6298ee6ac93f4d79a935fec1c67406f0cace99
Mohamed <mshareef@kiitos-tech.com>
2020-09-05 10:58:16 +0300

    Add material dialog and popup component to pages module #23

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
+// import { ButtonComponent } from '../shared/button/button.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PopupComponent } from '../shared/popup/popup.component';
 
 
 @NgModule({
@@ -9,6 +13,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     CommonModule,
     PagesRoutingModule,
-  ]
+    SharedModule,
+    MatDialogModule,
+  ],
+  entryComponents: [PopupComponent],
 })
 export class PagesModule { }
