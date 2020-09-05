12a977483e08a277ab9953d24d9d6ad50227157e
Mohamed <mshareef@kiitos-tech.com>
2020-09-05 10:58:38 +0300

    Create home page design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { MatDialog as MatDialogService } from '@angular/material/dialog';
+import { PopupComponent } from 'src/app/shared/popup/popup.component';
+import { DialogData } from 'src/app/interfaces';
 @Component({
   selector: 'spyfall-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  joinCodeControl: FormControl = new FormControl();
+
+  constructor(private dialogService: MatDialogService) { }
 
   ngOnInit(): void {
   }
+
+  createGame = (adminName: string) => {
+    console.log('admin name is: ', adminName);
+  }
+
+  onCreateGameClick = () => {
+    const data: DialogData = {
+      title: 'Create Game',
+      text: 'What is your name?',
+      confirmButtonText: 'Create',
+      rejectButtonText: 'Back',
+      confirmAction: this.createGame,
+    };
+    this.dialogService.open(PopupComponent, { data });
+  }
+
+  onJoinClicked = () => {
+    console.log('On Join Clicked');
+  }
+
 
 }
